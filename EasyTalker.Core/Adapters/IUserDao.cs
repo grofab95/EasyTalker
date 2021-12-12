678b0f75d7ce75f57b95986fb68883029b2f156b
Fabian <grochla95@gmail.com>
2021-12-12 20:38:42 +0100

    Cleanup solution

--- EasyTalker.Core/Adapters/IUserDao.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-using EasyTalker.Infrastructure.Dto.User;
-
-namespace EasyTalker.Core.Adapters;
-
-public interface IUserDao
-{
-    Task<UserDto> RegisterUser(string username, string password);
-}
