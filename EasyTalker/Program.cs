a904275fd198e0e2c39d635f594efed003cf0f0f
Fabian <fabian.grochla@gmail.com>
2022-06-18 23:18:59 +0200

    Fix solution dependencies

--- EasyTalker/Program.cs
+++ EasyTalker/Program.cs
@@ -1,12 +1,10 @@
 ﻿using EasyTalker.Api;
 using EasyTalker.Api.Extensions;
-using EasyTalker.Authentication.Database;
-using EasyTalker.Authentication.Database.Entities;
 using Serilog;
 using LoggerFactory = EasyTalker.Core.Logger.LoggerFactory;
 
 Log.Logger = LoggerFactory.Create();
-//FakeData();
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
 Startup.ConfigureServices(builder.Services);
@@ -23,19 +21,3 @@ var app = builder.Build();
 app.UseHttpsRedirection();
     
 app.ConfigureService().Run();
-
-void FakeData()
-{
-    var users = Enumerable.Range(1, 200)
-        .Select(x => new UserDb
-        {
-            IsActive = true,
-            Email = Faker.Internet.Email(),
-            UserName = Faker.Internet.UserName() 
-        });
-   
-    var ctx = new EasyTalkerAuthenticationContext();
-    
-    ctx.Users.AddRange(users);
-    ctx.SaveChanges();
-}
